b9440f3e0b128dfaacf9c48cbced6394abefaa9e
Steven Sarber <sesarber@gmail.com>
2024-06-27 22:54:03 -0700

    Added 404 pages. (User one doesn't work).

---
+++ app/users/[id]/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserNotFound = () => {
+  return (
+    <div>The requested user does not exist.</div>
+  )
+}
+
+export default UserNotFound
--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -6,6 +7,8 @@ interface Props {
 
 // destructuring the interface (pulling the id out) in function definition
 const UserDetails = ({ params: { id } }: Props) => {
+  if (id > 10) notFound();
+
   return <div>UserDetails {id}</div>;
 };
 
